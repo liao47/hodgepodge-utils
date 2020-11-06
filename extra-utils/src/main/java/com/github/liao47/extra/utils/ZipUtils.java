66d8c5b6dd3bf86467440aaa1d92c5fc89619b39
liao47 <liao647@foxmail.com>
2020-11-06 11:13:55 +0800

    解压缩工具类

--- extra-utils/src/main/java/com/github/liao47/extra/utils/ZipUtils.java
+++ extra-utils/src/main/java/com/github/liao47/extra/utils/ZipUtils.java
@@ -1,16 +1,21 @@
 package com.github.liao47.extra.utils;
 
+import com.github.liao47.common.exception.CustomException;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.lingala.zip4j.ZipFile;
+import net.lingala.zip4j.exception.ZipException;
 import net.lingala.zip4j.model.FileHeader;
+import net.lingala.zip4j.model.ZipParameters;
+import net.lingala.zip4j.model.enums.CompressionLevel;
+import net.lingala.zip4j.model.enums.CompressionMethod;
+import net.lingala.zip4j.model.enums.EncryptionMethod;
 import org.apache.commons.lang3.StringUtils;
 
 import java.io.File;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 /**
@@ -32,14 +37,22 @@ public class ZipUtils {
      */
     public static List<String> unzip(String zipFilePath, String outputDirPath, String password, Charset charset) {
         try {
-            File outputDir = new File(outputDirPath);
-            if (!outputDir.exists() && !outputDir.mkdirs()) {
-                log.warn("Make unzip output directories fail[{}]", outputDirPath);
+            if (StringUtils.isEmpty(outputDirPath)) {
+                outputDirPath = new File(zipFilePath).getParent();
+            } else {
+                File outputDir = new File(outputDirPath);
+                if (!outputDir.exists() && !outputDir.mkdirs()) {
+                    log.warn("Make unzip output directories fail[{}]", outputDirPath);
+                }
             }
 
             ZipFile zipFile = new ZipFile(zipFilePath);
             if (charset != null) {
                 zipFile.setCharset(charset);
+            }
+
+            if (!zipFile.isValidZipFile()) {
+                throw new CustomException("The zip file is invalid");
             }
 
             if (zipFile.isEncrypted() && StringUtils.isNotEmpty(password)) {
@@ -55,7 +68,73 @@ public class ZipUtils {
             return filePaths;
         } catch (Exception e) {
             log.error("Unzip file[{}] error, cause:", zipFilePath, e);
-            return Collections.emptyList();
+            throw new CustomException("Unzip file failure:" + e.getMessage());
         }
+    }
+
+    /**
+     * 压缩文件或文件夹
+     * @param src 要压缩的文件或文件夹路径
+     * @param dest 压缩文件存放路径
+     * @param password 压缩使用的密码
+     * @return
+     */
+    public static String zip(String src, String dest, String password) {
+        File srcFile = new File(src);
+        String destPath = buildDestZipFilePath(srcFile, dest);
+        ZipParameters parameters = new ZipParameters();
+        parameters.setCompressionMethod(CompressionMethod.DEFLATE);
+        parameters.setCompressionLevel(CompressionLevel.NORMAL);
+        if (!StringUtils.isEmpty(password)) {
+            parameters.setEncryptFiles(true);
+            parameters.setEncryptionMethod(EncryptionMethod.ZIP_STANDARD);
+        }
+        try {
+            ZipFile zipFile = new ZipFile(destPath);
+            if (parameters.isEncryptFiles()) {
+                zipFile.setPassword(password.toCharArray());
+            }
+            if (srcFile.isDirectory()) {
+                zipFile.addFolder(srcFile, parameters);
+            } else {
+                zipFile.addFile(srcFile, parameters);
+            }
+            return destPath;
+        } catch (ZipException e) {
+            log.error("Zip file error, cause:", e);
+            throw new CustomException("Zip file failure:" + e.getMessage());
+        }
+    }
+
+    /**
+     * 创建压缩文件路径
+     * @param srcFile
+     * @param dest
+     * @return
+     */
+    private static String buildDestZipFilePath(File srcFile, String dest) {
+        if (StringUtils.isEmpty(dest)) {
+            if (srcFile.isDirectory()) {
+                return srcFile.getParent() + File.separator + srcFile.getName() + ".zip";
+            }
+            return srcFile.getParent() + File.separator + StringUtils.substringBefore(srcFile.getName(), ".") + ".zip";
+        }
+
+        String destPath = dest;
+        File destDir = new File(dest);
+        if (destDir.isDirectory() || !destDir.getName().contains(".")) {
+            if (srcFile.isDirectory()) {
+                destPath += File.separator + srcFile.getName();
+            } else {
+                destPath += File.separator + StringUtils.substringBefore(srcFile.getName(), ".");
+            }
+            destPath += ".zip";
+        } else {
+            destDir = destDir.getParentFile();
+        }
+        if (!destDir.exists() && !destDir.mkdirs()) {
+            throw new CustomException("Make dest dirs failure");
+        }
+        return destPath;
     }
 }
---
+++ extra-utils/src/test/java/com/github/liao47/extra/utils/test/ZipUtilsTest.java
@@ -0,0 +1,42 @@
+package com.github.liao47.extra.utils.test;
+
+import com.github.liao47.extra.utils.ZipUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class ZipUtilsTest {
+
+    @Test
+    public void unzip() {
+        String zipFiles = "C:\\home\\test\\zipFiles\\a.zip";
+        List<String> list = ZipUtils.unzip(zipFiles, null, "pwd233", null);
+        assertFalse(list.isEmpty());
+
+        list = ZipUtils.unzip(zipFiles, "C:\\home\\test\\unzipFiles\\a", "pwd233", null);
+        assertFalse(list.isEmpty());
+
+        zipFiles = "C:\\home\\test\\zipFiles\\test_zip.zip";
+        list = ZipUtils.unzip(zipFiles, "C:\\home\\test\\unzipFiles", null, null);
+        assertFalse(list.isEmpty());
+    }
+
+    @Test
+    public void zip() {
+        String src = "C:\\home\\test\\test_zip";
+        String dest = "";
+        String zipPath = ZipUtils.zip(src, dest, null);
+        assertTrue(StringUtils.isNotEmpty(zipPath));
+
+        dest = "C:\\home\\test\\zipFiles\\";
+        zipPath = ZipUtils.zip(src, dest, null);
+        assertTrue(StringUtils.isNotEmpty(zipPath));
+
+        dest = "C:\\home\\test\\zipFiles\\a.zip";
+        zipPath = ZipUtils.zip(src, dest, "pwd233");
+        assertTrue(StringUtils.isNotEmpty(zipPath));
+    }
+}
