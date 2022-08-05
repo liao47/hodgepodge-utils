ee849064e2ebddeb060e2a6a7c41a910b94de5ee
liao47 <liao647@foxmail.com>
2022-08-05 10:50:53 +0800

    623. 在二叉树中增加一行

--- leetcode-test/src/main/java/com/github/liao47/leetcode/utils/HandyUtils.java
+++ leetcode-test/src/main/java/com/github/liao47/leetcode/utils/HandyUtils.java
@@ -22,6 +22,6 @@ public class HandyUtils {
     }
 
     public static void main(String[] args) {
-        System.out.println(humpTitle("minimum-subsequence-in-non-increasing-order"));
+        System.out.println(humpTitle("add-one-row-to-tree"));
     }
 }
--- leetcode-test/src/test/java/LeetCode0620To0629Test.java
+++ leetcode-test/src/test/java/LeetCode0620To0629Test.java
@@ -1,6 +1,11 @@
 import com.github.liao47.leetcode.P0622DesignCircularQueue;
+import com.github.liao47.leetcode.P0623AddOneRowToTree;
+import com.github.liao47.leetcode.bo.TreeNode;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author liaoshiqing
@@ -20,5 +25,17 @@ public class LeetCode0620To0629Test {
         Assert.assertTrue(queue.enQueue(4));
         Assert.assertEquals(4, queue.Rear());
         Assert.assertEquals(2, queue.Front());
+    }
+
+    @Test
+    public void test0623() {
+        P0623AddOneRowToTree solver = new P0623AddOneRowToTree();
+        TreeNode root = TreeNode.of(new Integer[]{4, 2, 6, 3, 1, 5});
+        List<Integer> ans = Arrays.asList(4, 1, 1, 2, null, null, 6, 3, 1, 5);
+        Assert.assertEquals(ans, solver.addOneRow(root, 1, 2).toList());
+
+        root = TreeNode.of(new Integer[]{4, 2, null, 3, 1});
+        ans = Arrays.asList(4, 2, null, 1, 1, 3, null, null, 1);
+        Assert.assertEquals(ans, solver.addOneRow(root, 1, 3).toList());
     }
 }
