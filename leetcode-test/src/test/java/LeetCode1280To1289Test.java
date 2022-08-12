d6a45e897fdcf1564f77d21fa03959d12d8846ca
liao47 <liao647@foxmail.com>
2022-08-12 11:22:57 +0800

    1282. 用户分组测试修改

--- leetcode-test/src/test/java/LeetCode1280To1289Test.java
+++ leetcode-test/src/test/java/LeetCode1280To1289Test.java
@@ -3,6 +3,7 @@ import com.alibaba.fastjson.TypeReference;
 import com.github.liao47.leetcode.P1282GroupThePeopleGivenTheGroupSizeTheyBelongTo;
 import org.junit.Assert;
 import org.junit.Test;
+import util.AssertUtils;
 
 import java.lang.reflect.Type;
 import java.util.Comparator;
@@ -18,17 +19,12 @@ public class LeetCode1280To1289Test {
         P1282GroupThePeopleGivenTheGroupSizeTheyBelongTo solver =
                 new P1282GroupThePeopleGivenTheGroupSizeTheyBelongTo();
         Type type = new TypeReference<List<List<Integer>>>() {}.getType();
-        List<List<Integer>> result = solver.groupThePeople(new int[]{3, 3, 3, 3, 3, 1, 3});
-        assert1282(JSON.parseObject("[[5],[0,1,2],[3,4,6]]", type), result);
-        result = solver.groupThePeople(new int[]{2, 1, 3, 3, 3, 2});
-        assert1282(JSON.parseObject("[[1],[0,5],[2,3,4]]", type), result);
-    }
-
-    private void assert1282(List<List<Integer>> expected, List<List<Integer>> actual) {
         Comparator<List<Integer>> comparator = (a, b) -> a.size() == b.size() ? a.get(0).compareTo(b.get(0)) :
                 Integer.compare(a.size(), b.size());
-        expected.sort(comparator);
-        actual.sort(comparator);
-        Assert.assertEquals(expected, actual);
+
+        List<List<Integer>> result = solver.groupThePeople(new int[]{3, 3, 3, 3, 3, 1, 3});
+        AssertUtils.assertList(JSON.parseObject("[[5],[0,1,2],[3,4,6]]", type), result, comparator);
+        result = solver.groupThePeople(new int[]{2, 1, 3, 3, 3, 2});
+        AssertUtils.assertList(JSON.parseObject("[[1],[0,5],[2,3,4]]", type), result, comparator);
     }
 }
--- leetcode-test/src/test/java/util/AssertUtils.java
+++ leetcode-test/src/test/java/util/AssertUtils.java
@@ -14,10 +14,14 @@ import java.util.List;
  */
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class AssertUtils {
-    public static void assertList(List<Integer> expected, List<Integer> actual) {
-        expected.sort(Comparator.comparingInt(a -> a));
-        actual.sort(Comparator.comparingInt(a -> a));
+    public static <T> void assertList(List<T> expected, List<T> actual, Comparator<T> comparator) {
+        expected.sort(comparator);
+        actual.sort(comparator);
         Assert.assertEquals(expected, actual);
+    }
+
+    public static void assertList(List<Integer> expected, List<Integer> actual) {
+        assertList(expected, actual, Comparator.comparingInt(a -> a));
     }
 
     public static void assertArray(int[] expected, int[] actual) {
