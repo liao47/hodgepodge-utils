a02c9f5c1e5b2688704cedada55a74feec2a20e6
liao47 <liao647@foxmail.com>
2022-08-25 17:44:16 +0800

    枚举工具优化

--- common-utils/src/main/java/com/github/liao47/utils/EnumUtils.java
+++ common-utils/src/main/java/com/github/liao47/utils/EnumUtils.java
@@ -1,8 +1,12 @@
 package com.github.liao47.utils;
 
+import com.github.liao47.common.exception.CustomException;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 
+import java.lang.reflect.Method;
 import java.util.Objects;
 import java.util.function.Function;
 
@@ -11,6 +15,7 @@ import java.util.function.Function;
  * @author liaoshiqing
  * @date 2022/8/19 17:19
  */
+@Slf4j
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class EnumUtils {
     /**
@@ -23,12 +28,70 @@ public class EnumUtils {
      * @return
      */
     public static <E extends Enum<E>, R> E getEnum(Class<E> enumClass, Function<E, R> valueMapper, R val) {
+        if (valueMapper == null) {
+            return getEnumByCode(enumClass, val);
+        }
         for (E e : enumClass.getEnumConstants()) {
             if (Objects.equals(val, valueMapper.apply(e))) {
                 return e;
             }
         }
         return null;
+    }
+
+    /**
+     * 通过字段值获取枚举，枚举不存在则抛异常
+     * @param enumClass
+     * @param valueMapper
+     * @param val
+     * @param nonExistentMsg
+     * @param <E>
+     * @param <R>
+     * @return
+     */
+    public static <E extends Enum<E>, R> E getEnum(Class<E> enumClass, Function<E, R> valueMapper, R val,
+                                                   String nonExistentMsg) {
+        E e = getEnum(enumClass, valueMapper, val);
+        if (e == null) {
+            throw new CustomException(StringUtils.defaultIfEmpty(nonExistentMsg, "枚举值有误"));
+        }
+        return e;
+    }
+
+    /**
+     * 通过code字段值获取枚举
+     * @param enumClass
+     * @param val
+     * @param <E>
+     * @return
+     */
+    public static <E extends Enum<E>> E getEnumByCode(Class<E> enumClass, Object val) {
+        try {
+            Method method = enumClass.getMethod("getCode");
+            for (E e : enumClass.getEnumConstants()) {
+                if (Objects.equals(val, method.invoke(e))) {
+                    return e;
+                }
+            }
+            return null;
+        } catch (NoSuchMethodException e) {
+            throw new CustomException(String.format("枚举【%s】里未找到getCode方法", enumClass.getSimpleName()));
+        } catch (Exception e) {
+            log.error("枚举【{}】反射调用getCode方法异常：", enumClass.getSimpleName(), e);
+            throw new CustomException(String.format("枚举【%s】反射调用getCode方法异常", enumClass.getSimpleName()));
+        }
+    }
+
+    /**
+     * 通过code字段值获取枚举，枚举不存在则抛异常
+     * @param enumClass
+     * @param val
+     * @param nonExistentMsg
+     * @param <E>
+     * @return
+     */
+    public static <E extends Enum<E>> E getEnumByCode(Class<E> enumClass, Object val, String nonExistentMsg) {
+        return getEnum(enumClass, null, val, nonExistentMsg);
     }
 
     /**
@@ -44,8 +107,12 @@ public class EnumUtils {
         if (enums == null || enums.length == 0) {
             return false;
         }
+        E v = getEnum(enums[0].getDeclaringClass(), valueMapper, val);
+        if (v == null) {
+            return false;
+        }
         for (E e : enums) {
-            if (Objects.equals(val, valueMapper.apply(e))) {
+            if (v == e) {
                 return true;
             }
         }
@@ -63,5 +130,27 @@ public class EnumUtils {
      */
     public static <E extends Enum<E>, R> boolean notIn(Function<E, R> valueMapper, R val, E... enums) {
         return !in(valueMapper, val, enums);
+    }
+
+    /**
+     * 判断code字段值是否在枚举中
+     * @param val
+     * @param enums
+     * @param <E>
+     * @return
+     */
+    public static <E extends Enum<E>> boolean in(Object val, E... enums) {
+        return in(null, val, enums);
+    }
+
+    /**
+     * 判断code字段值是否不在枚举中
+     * @param val
+     * @param enums
+     * @param <E>
+     * @return
+     */
+    public static <E extends Enum<E>> boolean notIn(Object val, E... enums) {
+        return !in(val, enums);
     }
 }
--- common-utils/src/test/java/com/github/liao47/utils/test/EnumUtilsTest.java
+++ common-utils/src/test/java/com/github/liao47/utils/test/EnumUtilsTest.java
@@ -1,5 +1,6 @@
 package com.github.liao47.utils.test;
 
+import com.github.liao47.common.exception.CustomException;
 import com.github.liao47.utils.EnumUtils;
 import com.github.liao47.utils.test.pojo.TaskState;
 import org.junit.Assert;
@@ -16,5 +17,16 @@ public class EnumUtilsTest {
         Assert.assertTrue(EnumUtils.in(TaskState::getCode, (byte) 0, TaskState.DRAFT, TaskState.DISABLE));
         Assert.assertFalse(EnumUtils.in(TaskState::getCode, (byte) 0, TaskState.ENABLE, TaskState.DISABLE));
         Assert.assertTrue(EnumUtils.notIn(TaskState::getCode, (byte) 0, TaskState.ENABLE, TaskState.DISABLE));
+
+        Assert.assertEquals(TaskState.DRAFT, EnumUtils.getEnumByCode(TaskState.class, (byte) 0));
+        Assert.assertTrue(EnumUtils.in((byte) 0, TaskState.DRAFT, TaskState.DISABLE));
+        Assert.assertFalse(EnumUtils.in((byte) 0, TaskState.ENABLE, TaskState.DISABLE));
+        Assert.assertTrue(EnumUtils.notIn((byte) 0, TaskState.ENABLE, TaskState.DISABLE));
+
+        Assert.assertFalse(EnumUtils.in(TaskState::getCode, TaskState.DRAFT, TaskState.DISABLE, TaskState.ENABLE));
+        Assert.assertFalse(EnumUtils.in(TaskState::getCode, TaskState.DRAFT));
+        Assert.assertTrue(EnumUtils.in((byte) 0, TaskState.DRAFT));
+        Assert.assertNull(EnumUtils.getEnumByCode(TaskState.class, (byte) -1));
+        Assert.assertThrows(CustomException.class, () -> EnumUtils.getEnumByCode(TaskState.class, (byte) -1, "状态值有误"));
     }
 }
