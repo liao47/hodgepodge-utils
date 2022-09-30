b5ee0f2b750e3ca17e3c77bf12434d75cb3018d8
liao47 <liao647@foxmail.com>
2022-09-30 15:40:42 +0800

    面试题 01.08. 零矩阵

---
+++ leetcode-test/src/main/java/com/github/liao47/leetcode/I0108ZeroMatrixLcci.java
@@ -0,0 +1,92 @@
+package com.github.liao47.leetcode;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 面试题 01.08. 零矩阵
+ * 编写一种算法，若M × N矩阵中某个元素为0，则将其所在的行与列清零。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：
+ * [
+ *   [1,1,1],
+ *   [1,0,1],
+ *   [1,1,1]
+ * ]
+ * 输出：
+ * [
+ *   [1,0,1],
+ *   [0,0,0],
+ *   [1,0,1]
+ * ]
+ * 示例 2：
+ *
+ * 输入：
+ * [
+ *   [0,1,2,0],
+ *   [3,4,5,2],
+ *   [1,3,1,5]
+ * ]
+ * 输出：
+ * [
+ *   [0,0,0,0],
+ *   [0,4,5,0],
+ *   [0,3,1,0]
+ * ]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/zero-matrix-lcci
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author liaoshiqing
+ * @date 2022/9/30 14:45
+ */
+public class I0108ZeroMatrixLcci {
+    public void setZeroes1(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        Set<Integer> col = new HashSet<>();
+        Set<Integer> row = new HashSet<>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    row.add(i);
+                    col.add(j);
+                }
+            }
+        }
+        for (Integer r : row) {
+            Arrays.fill(matrix[r], 0);
+        }
+        for (Integer c : col) {
+            for (int i = 0; i < m; i++) {
+                matrix[i][c] = 0;
+            }
+        }
+    }
+
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean[] row = new boolean[m];
+        boolean[] col = new boolean[n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = col[j] = true;
+                }
+            }
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (row[i] || col[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
--- leetcode-test/src/test/java/Interview01Test.java
+++ leetcode-test/src/test/java/Interview01Test.java
@@ -1,8 +1,11 @@
 import com.github.liao47.leetcode.I0101IsUniquelcci;
 import com.github.liao47.leetcode.I0102CheckPermutationLcci;
+import com.github.liao47.leetcode.I0108ZeroMatrixLcci;
 import com.github.liao47.leetcode.I0109StringRotationLcci;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.Arrays;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -25,6 +28,20 @@ public class Interview01Test {
         I0102CheckPermutationLcci solver = new I0102CheckPermutationLcci();
         Assert.assertTrue(solver.CheckPermutation("abc", "bca"));
         Assert.assertFalse(solver.CheckPermutation("abc", "bad"));
+    }
+
+    @Test
+    public void test0108() {
+        I0108ZeroMatrixLcci solver = new I0108ZeroMatrixLcci();
+        int[][] matrix = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+        int[][] answer = {{1, 0, 1}, {0, 0, 0}, {1, 0, 1}};
+        solver.setZeroes(matrix);
+        Assert.assertArrayEquals(answer, matrix);
+
+        matrix = new int[][]{{0, 1, 2, 0}, {3, 4, 5, 2}, {1, 3, 1, 5}};
+        answer = new int[][]{{0, 0, 0, 0}, {0, 4, 5, 0}, {0, 3, 1, 0}};
+        solver.setZeroes(matrix);
+        Assert.assertArrayEquals(answer, matrix);
     }
 
     @Test
